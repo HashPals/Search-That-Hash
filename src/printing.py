2a3161ea82e4506f6ad7802311cd081a2020982e
Jayy001 <56607897+Jayy001@users.noreply.github.com>
2021-01-27 12:50:10 +0000

    Update printing.py

--- src/printing.py
+++ src/printing.py
@@ -12,8 +12,11 @@ class Prettifier:
     This classes entire existence is to output stuff.
     """
 
-    def __init__(self, des):
-        self.des = des
+    def __init__(self, results, config):
+        self.banner()
+        self.pretty_print(results, config)
+
+    def banner(self):
         banner = """
    _____                     _        _______ _           _          _    _           _     
   / ____|                   | |      |__   __| |         | |        | |  | |         | |    
@@ -23,14 +26,24 @@ class Prettifier:
  |_____/ \\___|\\__,_|_|  \\___|_| |_|     |_|  |_| |_|\\__,_|\\__|      |_|  |_|\\__,_|___/_| |_|
                 """
         
-        self.test(des)
+        console.print(f"[bold blue]{banner}[/bold blue]")
+        console.print(f"""[bold underline blue]
+https://twitter.com/bee_sec_san
+https://github.com/HashPals/Name-That-Hash
+https://twitter.com/Jayy_2004 [/bold underline blue]""")
 
-
-    def test(self, des):
-        console.print(f"\n[bold #011627 on #ff9f1c]{des}[/bold #011627 on #ff9f1c]\n")
-        console.print("[bold #2ec4b6]No hashes found.[/bold #2ec4b6]")
-        console.print("\n[bold underline #2ec4b6]Least Likely[/bold underline #2ec4b6]\n")
-        console.print(f"[bold #e71d36]{des}[/bold #e71d36]")
-        consoleprint(f"[#ff9f1c]Summary: {des}[/#ff9f1c]")
-
-Prettifier("hello")
+    def pretty_print(self, results, config):
+        for hash, types in config["hashes"].items():
+            console.print(f"\n[bold #011627 on #ff9f1c]{hash}[/bold #011627 on #ff9f1c]\n")
+            console.print("\n[bold underline #2ec4b6]Results[/bold underline #2ec4b6]\n")
+            for name, result in results.items():
+                console.print(f"[#E71D36]{name}[/#E71D36] : {result}")
+            console.print("\n[bold underline #2ec4b6]Types[/bold underline #2ec4b6]\n")
+            types = [type["name"].lower() for type in types]
+            for i in range(len(types)):
+                console.print(f"[#E71D36]{types[i]}[/#E71D36]", end=", ")
+                if i == 5:
+                    break
+        console.print(f"\n\n")
+                
+    
