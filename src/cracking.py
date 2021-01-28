19cbdcd7ead89d303d77d589debd558c886e9d99
Jayy001 <56607897+Jayy001@users.noreply.github.com>
2021-01-28 15:15:23 +0000

    Update cracking.py

--- src/cracking.py
+++ src/cracking.py
@@ -1,11 +1,10 @@
-import warnings as logger
+from loguru import logger
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
 import requests
 from rich.console import Console
 
 console = Console()
-logger.filterwarnings("ignore")
 
 # This is an example input
 
@@ -16,6 +15,7 @@ logger.filterwarnings("ignore")
 class Searcher:
 
 	def __init__(self, config):
+		#logger.debug("called class searcher")
 		self.config = config
 		self.searchers_offline = [
 			hashcat()
@@ -28,7 +28,8 @@ class Searcher:
 			cmd5(),
 			md5_addr(),
 			md5_grom(),
-			sha1_grom()
+			sha1_grom(),
+			rainbow_tabels()
 		]
 		self.Hash_input = namedtuple(
 			"Hash_input", ["text", "types", "hashcats", "api_keys", "timeout"]
@@ -36,6 +37,7 @@ class Searcher:
 
 	def main(self):
 
+		#logger.debug("called main self")
 		return(self.perform_search(self.config))
 
 	def perform_search(self, config):
@@ -43,6 +45,7 @@ class Searcher:
 		out = []
 
 		if not config["timeout"]:
+			#logger.debug("Defulting timeout to 1")
 			config["timeout"] = 1
 
 		# print(config["timeout"])
@@ -53,7 +56,10 @@ class Searcher:
 			keys = [type["name"].lower() for type in types]
 			hashcats = [type["hashcat"] for type in types]
 
-			# print(keys)
+
+			#logger.debug(f"Possible types are {keys}")
+
+			#logger.debug(f"Called hash cracker {hash_ctext}")
 
 			supported_searchers = []
 			types = []
@@ -62,23 +68,30 @@ class Searcher:
 				for search in self.searchers_online:
 					for hashtype in keys:
 						if hashtype.lower() in search.supports:
-							supported_searchers.append(search)
-							types.append(hashtype)
-							break
+							if not search in supported_searchers:
+								supported_searchers.append(search)
+							if not hashtype in types:
+								types.append(hashtype)
 
 			for search in self.searchers_offline:
 				for hashtype in keys:
 					if hashtype.lower() in search.supports:
 						supported_searchers.append(search)
-						types.append(hashtype)
-						break
+						if not hashtype in types:
+							iypes.append(hashtype)
+
+			#logger.debug(f"Supported searchers are {supported_searchers}")
+			#logger.debug(f"Supported Types are {types}")
 
 			future = self.Hash_input(
 				hash_ctext, types, hashcats, config["api_keys"], config["timeout"]
 			)
 
+			#logger.debug(f"Future is {future}")
+
 			out.append(self.threaded_search(future, supported_searchers))
-				
+			
+			#logger.debug(f"Result is {out[-1]}")
 		return(out)
 
 	def threaded_search(self, future, supported_searchers):
@@ -97,13 +110,16 @@ class Searcher:
 			else:
 				success.update(d.result())
 		  #results.update(d)
-				return({future[0] : [success, fails]})
+
+		#logger.debug("Returned, ", {future[0] : [success, fails]})
+		return({future[0] : [success, fails]})
 
 	def call_searcher(self, search, future):
 		try:
+			#logger.debug(f"Called {type(search).__name__}")
 			return {type(search).__name__: search.crack(future)}
 		except Exception as e:
-			logger.warn(f"{type(search).__name__} [{e}]")
+			#logger.debug(f"{type(search).__name__} [{e}]")
 			return {type(search).__name__: "Failed"}
 
 
@@ -112,7 +128,7 @@ class Searcher:
 
 class hashsorg:
 
-	supports = set(["md5", "NTLM", "SHA-1"])
+	supports = set(["md5", "ntlm", "sha-1", "md4"])
 	moduels = ["requests"]
 	offline = False
 
@@ -124,16 +140,16 @@ class hashsorg:
 				f"https://hashes.org/api.php?key={key}&query={hash[0]}", timeout=hash[3]
 			).text
 		except:
-			logger.warn("Couldn't connect to hashesorg")
+			#logger.debug("Couldn't connect to hashesorg")
 			return("Not connected")
 
 		# Check for false positive
 
 		if "null" in request:
-			logger.warn("Couldnt connect to hashesorg")
+			#logger.debug("Couldnt connect to hashesorg")
 			return "Not connected"
 		if not request:
-			logger.warn("Hash seems to be a plain???")
+			#logger.debug("Hash seems to be a plain???")
 			return "Hash seems to be a plain'"
 
 		output = request.split('":"')[2].split('","')[0]
@@ -141,6 +157,7 @@ class hashsorg:
 		# Check for Hex
 
 		if "$HEX[" in output:
+			#logger.debug("Hex dected in hashesorg, converrting")
 			return bytearray.fromhex(
 				final_output[5 : len(final_output) - 1]
 			).decode()  # Partions it so that it only contains hex and then decodes it into ASCII
@@ -174,12 +191,12 @@ class hashcat:
 					"'hashcat' is not recognized as an internal or external command"
 					in str(error)
 				):
-					logger.warn("Hashcat not in PATH")
+					#logger.debug("Hashcat not in PATH")
 					return "Failed"
 				if "./hashcat.hctune: No such file or directory" in str(error):
-					logger.warn("Read the docs on using windows")
+					#logger.debug("Read the docs on using windows")
 					return "Failed"
-				logger.warn("Hashcat couldn't crack hash")
+				#logger.debug("Hashcat couldn't crack hash")
 				continue
 
 			possible_output = str(
@@ -249,56 +266,57 @@ class LmRainbowTabels:
 		}
 		try:
 			response = requests.request(
-				"POST", url, data=payload, headers=headers, timeout=hash[3]
+				"POST", url, data=payload, headers=headers, timeout=hash[3], verify=False
 			).text.split("&nbsp;")
 		except:
-			logger.warn("Couldn't connect to LM rainbow tabels")
+			#logger.debug("Couldn't connect to LM rainbow tabels")
 			return "Not connected"
 
 		if "CRACKED" in response[3]:
 			return response[5]
 
 		if "Not yet in database" in response[3]:
-			logger.warn("LmRainbowTabels couldnt crack hash")
+			#logger.debug("LmRainbowTabels couldnt crack hash")
 			return "Failed"
 
 		if "Uncrackable with this charset" in response[3]:
-			logger.warn("This isnt an LM hash.")
+			#logger.debug("This isnt an LM hash.")
 			return "Failed"
 
-
-# OPTIMIZATIONNNNN
 class md5crypt:
 	# From HashBuster https://github.com/s0md3v/Hash-Buster/blob/master/hash.py
-	supports = set(["md5", "md4", "ntlm", "sha-1", "sha-256", "sha384", "sha512", "ntlm"])
+	supports = set(["md5", "md4", "ntlm", "sha-1", "sha-256", "sha-384", "sha-512", "ntlm"])
 
-	def crack(self, hash):
+	def crack(self, hash): 
 		for type in hash[1]:
-			if type not in supports:
+			filtered_type = type.replace("-","")  # Md5crypt uses sha256, instead of sha-256 which is what NTH gives you
+			if type not in self.supports:
 				continue
-			res = self.search_one_type(hash[0], type)
-			if res != False:
-				return res
-		return False
+
+			result = self.search_one_type(hash, filtered_type)
+
+			if result:
+				return(result)
+
+		return("Failed")
 
 	def search_one_type(self, hash, type):
-		#print(f"Called, {hash}, {type}")
 		response = requests.get(
-			f"https://md5decrypt.net/Api/api.php?hash={hash}&hash_type={type}&email=deanna_abshire@proxymail.eu&code=1152464b80a61728",
-			timeout=hash[3],
+			f"https://md5decrypt.net/Api/api.php?hash={hash[0]}&hash_type={type}&email=deanna_abshire@proxymail.eu&code=1152464b80a61728", verify=False, timeout=hash[3]
 		).text
+
 		if len(response) != 0:
 			if "CODE ERREUR : 004" in response:
-				return False
+				return "Not Connected"
 			return response.strip("\n")
 		else:
-			return False
+			return None
 
 
 # Bug fixing, should be all good
 class nitrxgen:
 	# From HashBuster https://github.com/s0md3v/Hash-Buster/blob/master/hash.py
-	supports = set(["md5", "ntlm"])
+	supports = set(["md5"])
 
 	def crack(self, hash):
 		response = requests.get(
@@ -316,7 +334,7 @@ class nitrxgen:
 			return response
 
 		else:
-			logger.warn("Couldn't connect to nitrxegen or couldnt find a hash")
+			#logger.debug("Couldn't connect to nitrxegen or couldnt find a hash")
 			return "Failed"
 
 
@@ -328,18 +346,18 @@ class cmd5:
 
 		burp0_url = "https://www.cmd5.org:443/"
 		burp0_cookies = {
-			"ASP.NET_SessionId": "qlif5gdwpbwtgetadf1joxox",
-			"FirstVisit": "1/27/2021 2:50:19 AM",
-			"Hm_lvt_0b7ba6c81309fff7ce4498ec7b107c0b": "1611687026",
-			"Hm_lpvt_0b7ba6c81309fff7ce4498ec7b107c0b": "1611687026",
+			"ASP.NET_SessionId": "aaaaaaaaaaaaaaaaaaaaaaaa",
+			"FirstVisit": "28/01/2021 2:50:19 AM",
+			"Hm_lvt_0b7ba6c81309fff7ce4498ec7b107c0b": "0000000000",
+			"Hm_lpvt_0b7ba6c81309fff7ce4498ec7b107c0b": "0000000000",
 		}
 		burp0_headers = {
-			"Connection": "close",
-			"Cache-Control": "max-age=0",
-			"Upgrade-Insecure-Requests": "1",
+			"Connection": "open",
+			"Cache-Control": "max-age=1",
+			"Upgrade-Insecure-Requests": "0",
 			"Origin": "https://www.cmd5.org",
 			"Content-Type": "application/x-www-form-urlencoded",
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4281.88 Safari/537.36",
 			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 			"Sec-Fetch-Site": "same-origin",
 			"Sec-Fetch-Mode": "navigate",
@@ -363,16 +381,15 @@ class cmd5:
 
 		try:
 			text = requests.post(
-				burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data, timeout = hash[3]
+				burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data, timeout = hash[3], verify=False
 			).text
-			return "".join(
+			return("".join(
 				text.split(
 					'<span id="LabelAnswer" class="LabelAnswer" onmouseover="toggle();">'
 				)[1]
-			).split("<")[0]
+			).split("<")[0])
 		except:
-			return "Failed"
-
+			return("Failed")
 
 class md5_addr:
 
@@ -397,7 +414,7 @@ class md5_addr:
 
 		try:
 			text = requests.post(
-				burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data, timeout = hash[3]
+				burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data, timeout = hash[3], verify=False
 			).text
 			return "".join(
 				text.split(
@@ -413,7 +430,7 @@ class md5_grom:
 
 	def crack(self, hash):
 		try:
-			out = requests.get(f"https://md5.gromweb.com/?md5={hash[0]}", timeout = hash[3]).text
+			out = requests.get(f"https://md5.gromweb.com/?md5={hash[0]}", timeout = hash[3], verify=False).text
 			text = "".join(out.split('<input class="field" id="form_string_to_hash_string" type="search" name="string" value="')[1]).split('"')[0]
 			if not text:
 				return("Failed")
@@ -427,7 +444,7 @@ class sha1_grom:
 
 	def crack(self, hash):
 		try:
-			out = requests.get(f"https://sha1.gromweb.com/?hash={hash[0]}", timeout = hash[3]).text
+			out = requests.get(f"https://sha1.gromweb.com/?hash={hash[0]}", timeout = hash[3], verify=False).text
 			text = "".join(out.split('<input class="field" id="form_string_to_hash_string" type="search" name="string" value="')[1]).split('"')[0]
 			if not text:
 				return("Failed")
@@ -443,12 +460,30 @@ class opcrack:
 		try:
 			burp0_url = "https://cracker.okx.ch:443/crack"
 			burp0_headers = {"Connection": "open", "Accept": "application/json, text/javascript, */*; q=0.01", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", "Content-Type": "application/json", "Origin": "https://www.objectif-securite.ch", "Sec-Fetch-Site": "cross-site", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Referer": "https://www.objectif-securite.ch/", "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"}
-			burp0_json={"value": "0CB6948805F797BF2A82807973B89537"}
+			burp0_json={"value": hash[0]}
 
-			text = requests.post(burp0_url, headers=burp0_headers, json=burp0_json).text
+			text = requests.post(burp0_url, headers=burp0_headers, json=burp0_json, timeout = hash[3], verify=False).text
 
 			return("".join(text.split('":"')[1]).split('"}')[0])
 		except:
 			return("Not connected")
-			
+
+class private_keys:
+
+	supports = set(["sha-256"])
+
+	def crack(self, hash):
+		out = requests.get(f"https://privatekeys.pw/key/{hash[0]}", verify=False, timeout=hash[3]).text
+		print(out)
+
+class rainbow_tabels:
+
+	supports = set(["sha-256", "md5", "md4", "NTLM"])
+
+	def crack(self, hash):
+		try:
+			out = requests.get(f"https://hashdecryption.com/decrypt.php?str={hash[0]}&send=Submit", verify=False, timeout=hash[3]).text
+			return("".join(out.split("</b> is <b>")[1]).split("</b><br>")[0])
+		except:
+			return("Failed")
 # Will try and multi-thread later
