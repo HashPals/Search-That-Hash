3e8bd24f644ba891b15bc899ebaff99a710ab024
Jayy001 <56607897+Jayy001@users.noreply.github.com>
2021-01-30 16:13:48 +0000

    Create main_test.py

---
+++ tests/main_test.py
@@ -0,0 +1,25 @@
+from search_that_hash import main
+import click.testing
+
+def test_it_works():
+
+    hashes = ["5d41402abc4b2a76b9719d911017c592"]
+
+    x = main.return_as_json(hashes)
+    
+    assert x is not None
+
+
+def test_it_cracks_correctly():
+    hashes = ["098f6bcd4621d373cade4e832627b4f6"]
+
+    x = main.return_as_json(hashes)
+
+    assert "test" in x
+
+
+def test_main_succeeds():
+    runn = click.testing.CliRunner()
+    result = runn.invoke(main.main)
+
+    assert result.exit_code == 0
