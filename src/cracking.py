b392c681389c9e66f9919d7c4e66b363f2a5cad8
Jayy001 <56607897+Jayy001@users.noreply.github.com>
2021-01-30 16:11:31 +0000

    Update cracking.py
    Updated

--- src/cracking.py
+++ src/cracking.py
@@ -9,14 +9,15 @@ from concurrent.futures import ThreadPoolExecutor
 
 # Files
 
-from printing import Prettifier
-import online
-import offline
+try:
+    import printing, offline, online
+except ModuleNotFoundError:
+    from search_that_hash import offline, printing, online
+
 
 
 class Searcher:
     def __init__(self, config):
-        # logger.debug("called class searcher")
         self.config = config
         self.searchers_offline = [offline.hashcat()]
         self.searchers_online = [
@@ -32,12 +33,11 @@ class Searcher:
         ]
         self.Hash_input = namedtuple(
             "Hash_input",
-            ["text", "types", "hashcats", "api_keys", "timeout", "greppable"],
+            ["text", "types", "hashcat_types", "api_keys", "timeout", "greppable", "wordlist"],
         )
 
     def main(self):
 
-        # logger.debug("called main self")
         return self.perform_search(self.config)
 
     def perform_search(self, config):
@@ -45,21 +45,13 @@ class Searcher:
         out = []
 
         if not config["timeout"]:
-            # logger.debug("Defulting timeout to 1")
             config["timeout"] = 1
-
-        # print(config["timeout"])
 
         for hash, types in config["hashes"].items():
             hash_ctext = hash
 
             keys = [type["name"].lower() for type in types]
-            hashcats = [type["hashcat"] for type in types]
-
-            # logger.debug(f"Possible types are {keys}")
-
-            # logger.debug(f"Called hash cracker {hash_ctext}")
-
+            hashcat_types = [type["hashcat"] for type in types]
             supported_searchers = []
             types = []
 
@@ -72,30 +64,19 @@ class Searcher:
                             if not hashtype in types:
                                 types.append(hashtype)
 
-            for search in self.searchers_offline:
-                for hashtype in keys:
-                    if hashtype.lower() in search.supports:
-                        supported_searchers.append(search)
-                        if not hashtype in types:
-                            iypes.append(hashtype)
-
-            # logger.debug(f"Supported searchers are {supported_searchers}")
-            # logger.debug(f"Supported Types are {types}")
+            supported_searchers.extend([offline.hashcat(), offline.john()]) # Offline searchers
 
             future = self.Hash_input(
                 hash_ctext,
                 types,
-                hashcats,
+                hashcat_types, # For john, hashcat
                 config["api_keys"],
                 config["timeout"],
                 config["greppable"],
+                config["wordlist"]
             )
 
-            # logger.debug(f"Future is {future}")
-
             out.append(self.threaded_search(future, supported_searchers))
-
-            # logger.debug(f"Result is {out[-1]}")
 
         return out
 
@@ -112,7 +93,7 @@ class Searcher:
                     possible_done
                 ) in (
                     processes
-                ):  ## Okay so this basically checks the progress of everything during cracking :P
+                ):  ## Checks the progress of everything during cracking 
                     if possible_done in success or possible_done in fails:
                         continue
 
@@ -125,26 +106,19 @@ class Searcher:
                         success.update(possible_done.result())
 
                         if not future[
-                            5
-                        ]:  # Just prints it without waiting for everything to finish
-                            Prettifier.one_print(
+                            5 # Checks if greppable, if not then prints normally, if yes then returns and prints JSON.
+                        ]:  # Prints without waiting for other threads to finish.
+                            printing.Prettifier.one_print(
                                 str(list(possible_done.result().values())[0]), future[0]
                             )
                             return {
                                 future[0]: str(list(possible_done.result().values())[0])
                             }
-
-            if not future[5]:
-                Prettifier.one_print("Not Found", future[0])
-                return {future[0]: "Failed"}
-
-        # logger.debug("Returned, ", {future[0] : [success, fails]})
-        return {future[0]: [success, fadils]}
+                            
+        return {future[0]: [success, fails]}
 
     def call_searcher(self, search, future):
         try:
-            # logger.debug(f"Called {type(search).__name__}")
             return {type(search).__name__: search.crack(future)}
         except Exception as e:
-            # logger.debug(f"{type(search).__name__} [{e}]")
             return {type(search).__name__: "Failed"}
