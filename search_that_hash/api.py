f070aad64d575973cefb9d9628d5cbaade4210a0
Jayy001 <56607897+Jayy001@users.noreply.github.com>
2021-02-07 20:16:04 +0000

    Update api.py

--- search_that_hash/api.py
+++ search_that_hash/api.py
@@ -1,48 +1,47 @@
-# Don't use Config{} here, either update the dict orr store it all in the class
+import json
+from search_that_hash import cracking
+
 class Search_that_hash_api:
-    def __init__(self):
+    def __init__(self, config):
         config = {}
+        config["offline"] = False
+        config["api_keys"] = False
+        config["wordlist"] = False
+        config["hashcat"] = False
+        config["timeout"] = 1
+        config["greppable"] = True
+        config["binary"] = False
+        config["api"] = True
 
+    def create_hash_config(config):
+        # Gets the results from name-that-hash
+        return json.loads(nth.api_return_hashes_as_json(config["hashes"]))
+        
+    def fast(self, hashes):
 
-def return_as_json(hashes):
+        config = Search_that_hash_api.config
+        config["greppable"] = False
+        config["hashes"] = hashes
+        config["hashes"] = Search_that_hash_api.create_hash_config(config["hashes"])
 
-    config = {}
+        try:
+            searcher = cracking.Searcher(config)
+            return json.dumps(cracking.Searcher.main(searcher))
+        except:
+            return False
 
-    config["offline"] = False
-    config["api_keys"] = False
-    config["wordlist"] = False
-    config["hashcat"] = False
-    config["timeout"] = 1
-    config["greppable"] = True
-    config["hashes"] = hashes
-    config["binary"] = False
-    config["hashes"] = create_hash_config(config)
-    config["api"] = Flse
+    def info(self, hashes):
 
-    try:
-        searcher = cracking.Searcher(config)
-        return json.dumps(cracking.Searcher.main(searcher))
-    except:
-        return False
+        config = Search_that_hash_api.config
+        config["hashes"] = hashes
+        config["hashes"] = self.create_hash_config(config["hashes"])
 
+        try:
+            searcher = cracking.Searcher(config)
+            return json.dumps(cracking.Searcher.main(searcher))
+        except:
+            return False
+search = Search_that_hash_api()
+print(Search_that_hash_api.fast(search, "098f6bcd4621d373cade4e832627b4f6"))
 
-def return_as_fast_json(hashes):
-
-    config = {}
-
-    config["offline"] = False
-    config["api_keys"] = False
-    config["wordlist"] = False
-    config["hashcat"] = False
-    config["timeout"] = 1
-    config["greppable"] = False
-    config["hashes"] = hashes
-    config["binary"] = False
-    config["hashes"] = create_hash_config(config)
-    config["api"] = True
-
-    try:
-        searcher = cracking.Searcher(config)
-        return json.dumps(cracking.Searcher.main(searcher))
-    except:
-        return False
+# I cant get this to work feel free to just destory it all and rewrite it
