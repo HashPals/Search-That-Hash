1ae2ce948d9687633df63ee5fd944a31b7154fb7
SkeletalDemise <29117662+SkeletalDemise@users.noreply.github.com>
2021-02-27 06:21:13 -0800

    Fix pytest-cov

--- noxfile.py
+++ noxfile.py
@@ -53,7 +53,7 @@ def lint(session: Session) -> None:
 @nox.session(python=["3.8", "3.7"])
 def tests(session: Session) -> None:
     """Run the test suite."""
-    args = session.posargs or ["--cov", "-m", "not e2e"]
+    args = session.posargs or ["--cov", "--cov-fail-under=50", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
         session, "coverage[toml]", "pytest", "pytest-cov"
