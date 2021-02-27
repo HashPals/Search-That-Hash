861c9f10e6505db432cd6629c7e6acdc660f5c31
SkeletalDemise <29117662+SkeletalDemise@users.noreply.github.com>
2021-02-27 06:07:20 -0800

    Add back pytest-cov

--- noxfile.py
+++ noxfile.py
@@ -53,9 +53,11 @@ def lint(session: Session) -> None:
 @nox.session(python=["3.8", "3.7"])
 def tests(session: Session) -> None:
     """Run the test suite."""
-    args = session.posargs or ["-m", "not e2e"]
+    args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(session, "coverage[toml]", "pytest")
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov"
+    )
     session.run("pytest", *args)
 
 
